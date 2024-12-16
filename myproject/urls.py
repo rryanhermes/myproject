eaa84d9be67dee44166b7d856820dd431d73814e
Ryan Hermes <rryanhermes@gmail.com>
2024-12-16 01:33:36 -0600

    mroe stuff

--- myproject/urls.py
+++ myproject/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
-from myapp import views
+from . import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', views.index, name='index'),
     path('signup/', views.signup, name='signup'),  # Added the view for signup
     path('auth/', include('myapp.urls')),  # Include your app's URLs
+    path('testing/', views.testing, name='testing'),
+    path('login/', views.login, name='login'),
 ]
